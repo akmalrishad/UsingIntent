0b8091845655b65efbbef3b66e7d2d3f1127b7a2
akmalrishad <akmal.rishad.tif19@polban.ac.id>
2021-04-05 16:53:56 +0700

    first commit

---
+++ app/src/main/java/com/example/usingintent/MainActivity.java
@@ -0,0 +1,26 @@
+package com.akmal.usingintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int request_Code = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+        startActivityForResult(new Intent("com.akmal.usingintent.SecondActivity"),request_Code);
+    }
+    public void onActivityResult(int requestCode,int resultCode, Intent data){
+        if (requestCode == request_Code){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(this,data.getData().toString(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
